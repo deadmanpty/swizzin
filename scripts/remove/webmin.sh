824739602833c218b347255f03dd3b54faf3e88c
deadmanpty <deadmanpty@proton.me>
2025-06-02 09:14:01 +0100

    Update webmin.sh

--- scripts/remove/webmin.sh
+++ scripts/remove/webmin.sh
@@ -2,13 +2,13 @@
 # Webmin yeeter
 # flying_sausages 2020 for swizzin
 
-apt_remove webmin
+apt autoremove --purge webmin
 rm -rf /etc/webmin
-rm /etc/apt/sources.list.d/webmin.list
+rm /etc/apt/sources.list.d/webmin-stable.list
 
-if [[ -f /install/.nginx.lock ]]; then
-    rm /etc/nginx/apps/webmin.conf
-    systemctl reload nginx
-fi
+#if [[ -f /install/.nginx.lock ]]; then
+#    rm /etc/nginx/apps/webmin.conf
+#    systemctl reload nginx
+#fi
 
 rm /install/.webmin.lock
