4928acec793fd419dc4b346c102adbf7cca82c8d
deadmanpty <deadmanpty@proton.me>
2026-07-21 23:00:22 +0100

    Update webmin.sh

--- scripts/install/webmin.sh
+++ scripts/install/webmin.sh
@@ -20,18 +20,18 @@ deb [signed-by=/usr/share/keyrings/webmin-archive-keyring.gpg] https://download.
 EOF    
     echo_progress_done "Repo added"
     apt_update
-    apt install webmin
+    apt install webmin --install-recommends -y
 }
 
 _install_webmin
- if [[ -f /install/.nginx.lock ]]; then
-    echo_progress_start "Configuring nginx"
-    bash /etc/swizzin/scripts/nginx/webmin.sh
-    systemctl reload nginx
-    echo_progress_done
- else
-    echo_info "Webmin will run on port 10000"
- fi
+# if [[ -f /install/.nginx.lock ]]; then
+#    echo_progress_start "Configuring nginx"
+#    bash /etc/swizzin/scripts/nginx/webmin.sh
+#    systemctl reload nginx
+#    echo_progress_done
+# else
+#    echo_info "Webmin will run on port 10000"
+# fi
 
 echo_success "Webmin installed"
 echo_info "Please use any account with sudo permissions to log in"
